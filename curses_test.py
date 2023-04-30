83c084287fdfd45ccffd35d68cd24c66b81b4ed7
townsag <townsag@gmail.com>
2023-04-30 11:52:17 -0700

    fized the jumbled output problem by writing to a string buffer with console class and then reading from the string buffer and writting that to the terminal with the curses class

--- curses_test.py
+++ curses_test.py
@@ -1,4 +1,7 @@
 import curses
+import io
+import time
+
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
@@ -7,9 +10,9 @@ from rich import box
 def main():
 
     # initialize curses
-    # screen = curses.initscr()
-    # curses.noecho() # turn off key echoing
-    # curses.cbreak() # respond to keys immediately (don't wait for Enter)
+    screen = curses.initscr()
+    curses.noecho() # turn off key echoing
+    curses.cbreak() # respond to keys immediately (don't wait for Enter)
     # screen.keypad(True) # enable arrow keys
 
     # # set up initial position of character
@@ -37,7 +40,9 @@ def main():
     #     # clear the screen and move the cursor back to the top left
     #     screen.clear()
     #     screen.move(0, 0)
-    console = Console()
+
+
+    console = Console(file=io.StringIO())
     table = Table(
         title="My 4x4 Table",
         style="bright_white",
@@ -60,27 +65,39 @@ def main():
         #     cell.min_height = 5
     # Print the table
     console.print(table)
+    output = console.file.getvalue()
+    screen.addstr(f"size of output: {len(output)}")
+    screen.addstr(output)
+    screen.refresh()
+    time.sleep(5)
 
-    print("\n\n======\n")
-    with console.capture() as capture:
-        console.print(table)
-    capture_str = capture.get()
-    print("capture string length: ", len(capture_str))
-    for i in range(10):
-        print("a")
-    print("capture string type: ", type(capture_str))
-    print("num returns: ", len(capture_str.split("\n")))
-    print("printing line by line seprarated by ==========")
-    for line in capture_str.split("\n"):
-        print(line)
-        # print("==========")
+
+
+    # console.print(table)
+    #
+    # # print("\n\n======\n")
+    # screen.addstr("\n\n======\n")
+    # with console.capture() as capture:
+    #     console.print(table)
+    # capture_str = capture.get()
+    # # print("capture string length: ", len(capture_str))
+    # screen.addstr("capture string length: ", len(capture_str))
+    # # for i in range(10):
+    # #     print("a")
+    # # print("capture string type: ", type(capture_str))
+    # # print("num returns: ", len(capture_str.split("\n")))
+    # # print("printing line by line seprarated by ==========")
+    # for line in capture_str.split("\n"):
+    #     # print(line)
+    #     screen.addstr(line)
+    #     # print("==========")
 
 
     # # clean up curses before exiting
-    # curses.nocbreak()
+    curses.nocbreak()
     # screen.keypad(False)
-    # curses.echo()
-    # curses.endwin()
+    curses.echo()
+    curses.endwin()
 
 if __name__ == "__main__":
     main()
